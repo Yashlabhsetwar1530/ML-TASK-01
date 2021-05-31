f7ab11f168a265f5dbab53ccb6d02e3540601d6d
Yash Labhsetwar <70754364+Yashlabhsetwar1530@users.noreply.github.com>
2021-05-31 16:08:20 +0530

    Update ml_model.py

--- ml_model.py
+++ ml_model.py
@@ -12,4 +12,4 @@ y = df["Salary"]
 X_train, X_test,  y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)    #To split the data to 2 parts that is training and testing.
 mind = LinearRegression()                 #Start the machine learning model
 mind.fit(X_train ,y_train)                #Train the ML model
-dump(mind ,"SalaryModel.pkl")              #save as pickle file
+dump(mind ,"Salary_model.pkl")              #save as pickle file
